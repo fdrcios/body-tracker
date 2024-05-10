0859eaef68575fca41d49dfbbd84ba86447cafb7
Federico <fdrc.iosdev@gmail.com>
2024-05-10 11:19:40 +0200

    created xcode project

---
+++ Body Tracker/Body_TrackerApp.swift
@@ -0,0 +1,17 @@
+//
+//  Body_TrackerApp.swift
+//  Body Tracker
+//
+//  Created by Federico Cipriani on 10.05.24.
+//
+
+import SwiftUI
+
+@main
+struct Body_TrackerApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
